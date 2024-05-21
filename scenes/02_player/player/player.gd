1b9846e1e9c55fdcd8558d8a7031ee73b1db57a4
andresTapa <79794569+andresTapari@users.noreply.github.com>
2024-05-21 08:23:32 -0300

    coin added and player combo attack

--- scenes/02_player/player/player.gd
+++ scenes/02_player/player/player.gd
@@ -8,6 +8,9 @@ extends CharacterBody2D
 
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")	# Gravedad
 var stateMachine																# Maquina de estados	
+var attackEn = false
+var groundCombo = 0
+var airCombo 	= 0
 
 func _ready() -> void:
 	# Cargamos maquina de estado
@@ -17,20 +20,37 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	# Agregamos gravedad
+	apply_gravity(delta)
+
+	# Administramos salto
+	handle_jump()
+	
+	# Administramos movimientos
+	handle_movement()
+	
+	# Administramos ataque
+	handle_attack()
+
+	# Movemos el personaje
+	move_and_slide()
+
+func apply_gravity(delta) -> void:
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Administramos salto
+func handle_jump() -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = jumpVelocity
-
 	# actualizamos animaciones
 	if velocity.y > 0:
 		stateMachine.travel("fall")
 	elif velocity.y < 0:
 		stateMachine.travel("jump")
-	
-	# Obtenemos entradas de jugador:
+
+func handle_movement() -> void:
+	if attackEn:
+		return
+	# Obtenemos entradas de movimiento del jugador:
 	var direction := Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left") 
 	if direction:
 		velocity.x = direction * speed
@@ -43,5 +63,37 @@ func _physics_process(delta: float) -> void:
 		if is_on_floor():
 			stateMachine.travel("idle")
 
-	# Movemos el personaje
-	move_and_slide()
+func handle_attack() -> void:
+	if Input.is_action_just_pressed("ui_attack"):
+		attackEn = true
+		%ComboTimer.start()
+		if not %ComboTimer.is_stopped():
+			groundCombo += 1
+		if is_on_floor():
+			#groundAttackCounter += 1
+			match groundCombo:
+				1:
+					stateMachine.travel("attack_1")
+				2:
+					stateMachine.travel("attack_2")
+				3:
+					stateMachine.travel("attack_3")
+		#else:
+			#airAttackCounter += 1
+			#match airAttackCounter:
+				#1:
+					#stateMachine.travel("air_attack_1")
+				#2:
+					#stateMachine.travel("air_attack_2")
+	#
+			#velocity.y = jumpVelocity/2
+
+func attack() -> void:
+
+	print("Attack")
+	attackEn = false
+	pass
+
+
+func _on_animation_tree_animation_finished(anim_name):
+	pass # Replace with function body.
---
+++ scenes/03_coins/coin.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+signal picked(score)
+
+## Puntaje de la moneta
+@export var score = 1
+
+## Se ejecuta cuando un cuerpo entra en el área de detección de la moneda
+func _on_body_entered(body):
+	# Si body pertenece al grupo player
+	if body.is_in_group("player"):
+		# Emitimos señal que se recolecto una moneda
+		picked.emit(score)
+		# Reproducimos animación
+		$AnimatedSprite2D.play("picked")
+		# Conectamos señal que termina animación para eliminar la moneda
+		$AnimatedSprite2D.animation_finished.connect(func (): queue_free())
