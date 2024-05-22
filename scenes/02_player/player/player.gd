3de458187afa0f399c3461159d83add33aa5887f
andresTapa <79794569+andresTapari@users.noreply.github.com>
2024-05-22 12:50:28 -0300

    animation tree fixed

--- scenes/02_player/player/player.gd
+++ scenes/02_player/player/player.gd
@@ -12,7 +12,7 @@ var SWORD = preload(Globals.THROW_SWORD_PATH)
 
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")	# Gravedad
 var stateMachine																# Maquina de estados	
-var attackEn = false
+#var attackEn = false
 var groundAttackNames = ["attack_1","attack_2","attack_3"]
 var airAttackNames = ["air_attack_1","air_attack_2"]
 var groundCombo = 0
@@ -41,8 +41,8 @@ func apply_gravity(delta) -> void:
 		velocity.y += gravity * delta
 
 func handle_jump() -> void:
-	if attackEn:
-		return
+	#if attackEn:
+		#return
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = jumpVelocity
 	# actualizamos animaciones
@@ -52,8 +52,8 @@ func handle_jump() -> void:
 		stateMachine.travel("jump")
 
 func handle_movement() -> void:
-	if attackEn:
-		return
+	#if attackEn:
+		#return
 	# Obtenemos entradas de movimiento del jugador:
 	var direction := Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left") 
 	if direction:
@@ -69,7 +69,7 @@ func handle_movement() -> void:
 
 func handle_attack() -> void:
 	if Input.is_action_just_pressed("ui_attack"):
-		attackEn = true
+		#attackEn = true
 		if is_on_floor():
 			stateMachine.travel(groundAttackNames[groundCombo])
 			groundCombo = groundCombo + 1 if groundCombo < 2 else 0
@@ -81,7 +81,7 @@ func handle_attack() -> void:
 		stateMachine.travel("throw_sword")
 
 func attack() -> void:
-	attackEn = false
+	#attackEn = false
 	%RayCast2D.force_raycast_update()
 	var collider  = %RayCast2D.get_collider()
 	if collider:
