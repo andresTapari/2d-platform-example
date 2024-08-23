e5912c993e0eab52d98c69e6836fc507535fb0af
andresTapa <79794569+andresTapari@users.noreply.github.com>
2024-08-23 11:50:21 -0300

    barrel.gd refactorized

--- scenes/06_breackableProps/barrel/barrel.gd
+++ scenes/06_breackableProps/barrel/barrel.gd
@@ -5,10 +5,12 @@ extends RigidBody2D
 ## Fuerza de salto cuando es golpeado
 @export var jumpForce: 	int = 100
 
+## Partes del barril
 @onready var BARREL_PARTS = preload(Globals.BARREL_PARTS_TSCN)
 
 var currentHealth = health	# Vida del barril
-## 
+
+## Ejecutar función de 
 func hit(incomeDamage: int, incomeDamagePosition: Vector2 = Vector2.ZERO) -> void:
 	# Calculamos Impulso
 	var dir : Vector2 = (global_position - incomeDamagePosition).normalized()
@@ -16,24 +18,25 @@ func hit(incomeDamage: int, incomeDamagePosition: Vector2 = Vector2.ZERO) -> voi
 	# Descontar vida
 	currentHealth -= incomeDamage
 	apply_impulse(impulse)
+	
 	# Actualizamos animacion
 	%AnimatedSprite2D.play("hit")
+	$soundFx/hurt.play()
+	
+	# Evaluamos vida actual
 	if currentHealth <= 0:
 		$soundFx/explosion.play()
+		$soundFx/explosion.finished.connect(func (): call_deferred("queue_free"))
 		%AnimatedSprite2D.visible = false
 		for n in range(4):
 			var newPart:BarrelPart = BARREL_PARTS.instantiate()
-			#get_parent().call_deferred("add_child",newPart)
 			get_parent().add_child(newPart)
 			newPart.global_position = self.global_position
 			newPart.spriteFrame = n
 			var newImpulse = Vector2(jumpForce,-jumpForce) if n%2 else Vector2(-jumpForce,-jumpForce)
 			if n>1:
 				newImpulse.y = 0
-			newPart.apply_impulse(newImpulse*2)
-		$soundFx/explosion.finished.connect(func (): call_deferred("queue_free"))
-	else:
-		$soundFx/hurt.play()
+			newPart.apply_impulse(newImpulse * 1.5)
 	
 	# Esperamos que la animacion termine
 	await %AnimatedSprite2D.animation_finished
